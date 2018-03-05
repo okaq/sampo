d0920bb5ba56c9095043cf1628dfdb460164ce6c
AQ <aq@okaq.com>
2018-03-05 09:30:00 -0500

    File paths from names

--- amo/amo.go
+++ amo/amo.go
@@ -6,15 +6,28 @@ package main
 import (
     "fmt"
     "net/http"
+    "os"
 )
 
 const (
     // try open file for writing
     // create new if it doesnt exist
     CFG = "cfg.txt"
-    SRT = "sat.txt"
+    SRT = "srt.txt"
     PAT = "data/"
 )
+
+var (
+    Scratch *os.File
+    Config *os.File
+)
+
+func Load() {
+    // global state
+    p0 := fmt.Sprintf("%s%s", PAT, CFG)
+    p1 := fmt.Sprintf("%s%s", PAT, SRT)
+    fmt.Println(p0, p1)
+}
 
 func Handler(w http.ResponseWriter, r *http.Request) {
     fmt.Println(r)
@@ -23,8 +36,11 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 func main() {
     fmt.Println("starting amo web")
     fmt.Println("opening data files")
+    Load()
     // server
     // stats handler
     // config load
     // scratch load
 }
+
+
