5a42058e470c91d3ee22810afeec544b88036f89
AQ <aq@okaq.com>
2018-03-19 09:26:11 -0400

    Atomic counter cache

--- amo/chanto.go
+++ amo/chanto.go
@@ -6,24 +6,50 @@ package main
 import (
     "fmt"
     "net/http"
+    "sync/atomic"
 )
 
 const (
     INDEX = "chanto.html"
 )
 
+var (
+    Counter uint64
+)
+
 func ChanHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println(r)
+    // increment counter
+    _ = atomic.AddUint64(&Counter, 1)
     http.ServeFile(w, r, INDEX)
 }
 
 func StatHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println(r)
     // pipeline atomic counter
+    c0 := atomic.LoadUint64(&Counter)
+    // json response
+    c1 := struct {
+        Count uint64 `json:"count"`
+    } {
+        c0
+    }
+    j0, err := json.Marshal(c1)
+    if err != nil {
+        fmt.Println(err)
+    }
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(j0)
+}
+
+func Count() {
+    // init counter
+    Counter = 0
 }
 
 func main() {
     fmt.Println("web serving on localhost:8080")
+    Count()
     http.HandleFunc("/", ChanHandler)
     http.HandleFunc("/s", StatHandler)
     http.ListenAndServe(":8080", nil)
