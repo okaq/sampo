3876bbe4d744307ee5c44112d359f6faa5931db2
AQ <aq@okaq.com>
2018-04-18 17:41:17 -0400

    Json and bitmap handlers

--- amo/eiga.go
+++ amo/eiga.go
@@ -12,6 +12,8 @@ import (
 
 const (
     PATH = "data/"
+    JSON = "data/json"
+    IMG = "data/img"
     INDEX = "eiga.html"
 )
 
@@ -30,11 +32,25 @@ func EigaHandler(w http.ResponseWriter, r *http.Request) {
     http.ServeFile(w,r,INDEX)
 }
 
+func ImgHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    // endpoint for bitmap storage
+}
+
+func JsonHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    // endpoint for json storage
+}
+
 func main() {
     t0 := time.Now()
     fmt.Printf("okaq eiga web serve started at %s\n", t0.String())
     Rando()
     fmt.Printf("session id %d\n", Rng.Int())
+    fmt.Printf("json path located at %s\n", JSON)
+    fmt.Printf("img path located at %s\n", IMG)
     http.HandleFunc("/", EigaHandler)
+    http.HandleFunc("/a", ImgHandler)
+    http.HandleFunc("/b", JsonHandler)
     http.ListenAndServe(":8080", nil)
 }
