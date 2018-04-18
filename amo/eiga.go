3ccc4443ea9feb860bdc2695561736660abdae5a
AQ <aq@okaq.com>
2018-04-18 10:20:38 -0400

    Web session start

--- amo/eiga.go
+++ amo/eiga.go
@@ -1,5 +1,5 @@
 // okaq sampo eiga
-// bitmap font smapler
+// bitmap font sampler
 // 2018-04-20
 package main
 
@@ -19,10 +19,22 @@ var (
     Rng *rand.Rand
 )
 
+func Rando() {
+    t0 := time.Now().UnixNano()
+    s0 := rand.NewSource(t0)
+    Rng = rand.New(s0)
+}
+
+func EigaHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    http.ServeFile(w,r,INDEX)
+}
+
 func main() {
     t0 := time.Now()
-    fmt.Println("okaq eiga web serve started at %s", t0.ToString())
+    fmt.Printf("okaq eiga web serve started at %s\n", t0.String())
     Rando()
+    fmt.Printf("session id %d\n", Rng.Int())
     http.HandleFunc("/", EigaHandler)
     http.ListenAndServe(":8080", nil)
 }
