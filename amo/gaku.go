14dc1a5908f20ecc7a8e33dbbc173356d5ac0d8e
AQ <aq@okaq.com>
2018-05-08 21:27:17 -0400

    Image file name cache

--- amo/gaku.go
+++ amo/gaku.go
@@ -6,12 +6,22 @@ package main
 
 import (
     "fmt"
+    "io/ioutil"
     "net/http"
     "time"
 )
 
 const (
     INDEX = "gaku.html"
+    // imge dir path
+    IMG = "img/"
+    // data dir path
+    DATA = "data/"
+)
+
+var (
+    // file list
+    Images []string
 )
 
 func motd() {
@@ -19,14 +29,37 @@ func motd() {
     fmt.Printf("OKAQ web server start\n%s\n", s0)
 }
 
+func files() {
+    // read asset files into data cache
+    f0, err := ioutil.ReadDir(IMG)
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Printf("Found %d image files\n", len(f0))
+    Images = make([]string, len(f0))
+    for i,f1 := range f0 {
+        fmt.Println(f1.Name)
+        Images[i] = f1.Name
+    }
+}
+
 func GakuHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println(r)
     http.ServeFile(w,r,INDEX)
 }
 
+func ImgHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    // static png server for import into sampler
+    fmt.Println(http.StripPrefix(r.URL))
+    // http.ServeFile(w,r,IMG+png)
+}
+
 func main() {
     motd()
+    files()
     http.HandleFunc("/", GakuHandler)
+    http.HandleFunc("/a", ImgHandler)
     http.ListenAndServe(":8080", nil)
 }
 
