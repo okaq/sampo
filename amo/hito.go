57ea07ca9ac47ad1fd1eb20ec4975fc5402d6ad8
AQ <aq@okaq.com>
2018-05-21 09:34:10 -0400

    Image file paths list

--- amo/hito.go
+++ amo/hito.go
@@ -6,13 +6,35 @@ package main
 
 import (
     "fmt"
+    "io/ioutil"
     "net/http"
     "time"
 )
 
 const (
     INDEX = "hito.html"
+    // json file path
+    IMGB = "imgb/"
 )
+
+var (
+    F []string
+)
+
+func files() {
+    F = make([]string, 0)
+    f0, err := ioutil.ReadDir(IMGB)
+    if err != nil {
+        fmt.Println(err)
+    }
+    for _, f := range f0 {
+        // fmt.Println(f.Name())
+        // fmt.Printf("%d: %s%s\n", i, IMGB, f.Name())
+        s0 := fmt.Sprintf("%s%s", IMGB, f.Name())
+        F = append(F, s0)
+    }
+    fmt.Println(F)
+}
 
 func HitoHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Println(r)
@@ -22,6 +44,7 @@ func HitoHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
     fmt.Printf("okaq hito start on localhost:8080\n%s\n", time.Now().String())
     // do files
+    files()
     http.HandleFunc("/", HitoHandler)
     http.ListenAndServe(":8080", nil)
 }
