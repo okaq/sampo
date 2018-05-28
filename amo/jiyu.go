ff333bc9de46c575fd7fec690426593173e184b7
AQ <aq@okaq.com>
2018-05-28 08:18:04 -0400

    New file jiyu.html

---
+++ amo/jiyu.go
@@ -0,0 +1,33 @@
+// okaq web server
+// "the cookie problem"
+// AQ <aq@okaq.com>
+// 2018-05-30
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "time"
+)
+
+const (
+    INDEX = "jiyu.html"
+)
+
+func motd() {
+    fmt.Println("okaq starting on localhost:8080")
+    fmt.Printf("%s\n", time.Now().ToString())
+}
+
+func JiyuHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    http.ServeFile(w,r,INDEX)
+}
+
+func main() {
+    motd()
+    http.HandleFunc("/", JiyuHandler)
+    http.ListenAndServe(":8080", nil)
+}
+
+
