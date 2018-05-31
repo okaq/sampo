234a05f3891ca3cf4612dca90cf780ba36035383
AQ <aq@okaq.com>
2018-05-30 21:03:46 -0400

    Jar handler for server game state

--- amo/jiyu.go
+++ amo/jiyu.go
@@ -29,10 +29,17 @@ func StatHandler(w http.ResponseWriter, r *http.Request) {
     // accumulator
 }
 
+func JarHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r)
+    // jars kept in global state on server
+    // session key on client can be used to decrypt
+}
+
 func main() {
     motd()
     http.HandleFunc("/", JiyuHandler)
     http.HandleFunc("/a", StatHandler)
+    http.HandleFunc("/b", JarHandler)
     http.ListenAndServe(":8080", nil)
 }
 
