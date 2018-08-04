21c5877a43d5f6cc999c879537a36812c70f3aa5
AQ <aq@okaq.com>
2018-08-04 22:13:33 +0000

    New file moko.html

---
+++ amo/moko.go
@@ -0,0 +1,33 @@
+// okaq web
+// gltf load
+// AQ <aq@okaq.com>
+// 2018-08-06
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+const (
+	INDEX = "moko.html"
+)
+
+func MokoHandler(w http.Responsewriter, r *http.Request) {
+	fmt.Println(r)
+	http.ServeFile(w,r,INDEX)
+}
+
+func motd() {
+	fmt.Println("okaq gltf started localhost:8080")
+	fmt.Printf("start: %s\n", time.Now().String())
+}
+
+func main() {
+	motd()
+	http.HandleFunc("/", MokoHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+
